c36db7e47b660e02d54c6859a458299609e9652c
Barath K <127032804+barath-sk17@users.noreply.github.com>
2024-07-14 17:43:33 +0530

    e-vault

---
+++ e-vault/src/components/menu/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import '../../assets/css/footer.css';
+
+const Footer = () => (
+  <div className="footer page-footer font-small blue">
+  </div>
+);
+
+export default Footer;
